73ae80432f5c23a3ae6f0ad0fb970fe6f554a3bf
RajDhruv <rajdhruv30@gmail.com>
2019-01-15 01:31:56 +0530

    All Changes for deployement

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,7 @@
 class Blog < ApplicationRecord
 
 	after_create :create_post
+	belongs_to :user, class_name: 'User', foreign_key: 'created_by'
 
 	def create_post
 		@post=Post.new
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,3 @@
 class Post < ApplicationRecord
+	belongs_to :user, class_name: 'User', foreign_key: 'created_by'
 end
