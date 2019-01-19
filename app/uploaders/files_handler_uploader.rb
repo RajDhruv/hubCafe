3dd5afafb176f7dcf516e72522e24bcde1cb9920
RajDhruv <rajdhruv30@gmail.com>
2019-01-19 19:11:01 +0530

    Uploader Path Unpublic

--- app/uploaders/files_handler_uploader.rb
+++ app/uploaders/files_handler_uploader.rb
@@ -9,11 +9,14 @@ class FilesHandlerUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
+  # CarrierWave.configure do |config|
+  #   config.root = Rails.root
+  # end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "#{Rails.root}/private/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
